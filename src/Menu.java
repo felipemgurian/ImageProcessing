1394434990dfa2e8a14b9ad6386ed1031760b2d5
unknown <felipemgurian@hotmail.com>
2020-06-06 21:40:24 -0300

    Add Title and new Icon

--- src/Menu.java
+++ src/Menu.java
@@ -6,6 +6,8 @@ import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
+import java.net.URL;
+
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JDesktopPane;
@@ -36,11 +38,8 @@ public class Menu extends JFrame {
 	public Menu() {
 	    Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 		
-		this.setPreferredSize(new Dimension(screenSize.width, screenSize.height));
-		this.pack();
-	    this.setLocationRelativeTo(null);
-	    
-		getContentPane().setBackground(Color.WHITE);
+
+		getContentPane().setBackground(Color.WHITE);;
 		getContentPane().setForeground(Color.BLACK);
 		getContentPane().setEnabled(false);
 		getContentPane().setLayout(null);
@@ -53,27 +52,27 @@ public class Menu extends JFrame {
 		 * Central Content Panel
 		 */
 		
-		JPanel panel_1 = new JPanel();
-		panel_1.setBounds(136, 11, (int) (screenSize.width * 0.72), (int) (screenSize.height * 0.87));
-		panel_1.repaint();
-		getContentPane().add(panel_1);
+		JPanel contentPanel = new JPanel();
+		contentPanel.setBounds(136, 11, (int) (screenSize.width * 0.72), (int) (screenSize.height * 0.87));
+		contentPanel.repaint();
+		getContentPane().add(contentPanel);
 		
 		/**
 		 * SIDE BUTTOM BAR
 		 */
 		
-		JPanel panel = new JPanel();
-		panel.setBackground(Color.WHITE);
-		panel.setBounds(0, 0, (int) (screenSize.width * 0.10),  (int) (screenSize.height * 0.87));
-		getContentPane().add(panel);
-		panel.setLayout(null);
+		JPanel sidePanel = new JPanel();
+		sidePanel.setBackground(Color.WHITE);
+		sidePanel.setBounds(0, 0, (int) (screenSize.width * 0.10),  (int) (screenSize.height * 0.87));
+		getContentPane().add(sidePanel);
+		sidePanel.setLayout(null);
 		
 		txtActions = new JTextField();
 		txtActions.setEditable(false);
 		txtActions.setHorizontalAlignment(SwingConstants.CENTER);
 		txtActions.setText("Actions");
 		txtActions.setBounds(20, 11, 86, 20);
-		panel.add(txtActions);
+		sidePanel.add(txtActions);
 		txtActions.setColumns(10);
 		
 		JButton btnBt = new JButton("");
@@ -84,30 +83,30 @@ public class Menu extends JFrame {
 		});
 		btnBt.setForeground(Color.WHITE);
 		btnBt.setBounds(20, 41, 38, 38);
-		panel.add(btnBt);
+		sidePanel.add(btnBt);
 		
 		JButton button = new JButton("");
 		button.setIcon(new ImageIcon(ProjectPath + "\\Resources\\Img\\Lapiz.png"));
 		button.setBounds(68, 41, 38, 38);
-		panel.add(button);
+		sidePanel.add(button);
 		
 		JButton button_1 = new JButton("");
 		button_1.setIcon(new ImageIcon(ProjectPath + "\\Resources\\Img\\Borracha.png"));
 		button_1.setForeground(Color.WHITE);
 		button_1.setBounds(20, 90, 38, 38);
-		panel.add(button_1);
+		sidePanel.add(button_1);
 		
 		JButton button_2 = new JButton("");
 		button_2.setIcon(new ImageIcon(ProjectPath + "\\Resources\\Img\\Lupa.png"));
 		button_2.setForeground(Color.WHITE);
 		button_2.setBounds(68, 90, 38, 38);
-		panel.add(button_2);
+		sidePanel.add(button_2);
 		
 		JButton button_3 = new JButton("");
 		button_3.setIcon(new ImageIcon(ProjectPath + "\\Resources\\Img\\Balde.png"));
 		button_3.setForeground(Color.WHITE);
 		button_3.setBounds(20, 139, 38, 38);
-		panel.add(button_3);
+		sidePanel.add(button_3);
 		
 		
 	    /**
@@ -157,8 +156,8 @@ public class Menu extends JFrame {
 	                Image img;
 					try {
 						img = new Image(jfc.getSelectedFile());
-						panel_1.add(img, BorderLayout.CENTER);
-		                panel_1.repaint();
+						contentPanel.add(img, BorderLayout.CENTER);
+		                contentPanel.repaint();
 					} catch (IOException e1) {
 						// TODO Auto-generated catch block
 						e1.printStackTrace();
@@ -174,6 +173,14 @@ public class Menu extends JFrame {
 		 * JFRAME CONFIG
 		 */
 	    
+		this.setTitle("P2 Sistemas Multimidia e Jogos Digitais - 2020");
+		
+		URL url = this.getClass().getResource("Resources\\Img\\Icon.png");
+		java.awt.Image titleImage = Toolkit.getDefaultToolkit().getImage(url);
+		this.setIconImage(titleImage);
+		
+		this.pack();
+	    this.setLocationRelativeTo(null);
 	    this.setExtendedState(JFrame.MAXIMIZED_BOTH); 
 		this.setVisible(true);
 	}
